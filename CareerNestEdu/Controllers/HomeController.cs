721cc5d77c5d1acfa56574fd89857690961cc3d6
Roshan2422 <Roshan@softvellytechnology.com>
2025-10-31 10:28:47 +0530

    features pages added

--- CareerNestEdu/Controllers/HomeController.cs
+++ CareerNestEdu/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace CareerNestEdu.Controllers
         {
             return View();
         }
+        public IActionResult Features()
+        {
+            return View();
+        }
 
         public IActionResult LoginSignup()
         {
