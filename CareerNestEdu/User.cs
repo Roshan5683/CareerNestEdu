6c278c8ce80eab5395ae05af769b729fa46c3742
Gyani kumar <116051737+GyaniSingh0885@users.noreply.github.com>
2025-10-31 13:56:18 +0530

    Add files via upload
    model users

---
+++ CareerNestEdu/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CareerNestEdu.Models;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string EmailAddress { get; set; } = null!;
+
+    public string PhoneNumber { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string ConfirmPassword { get; set; } = null!;
+
+    public DateTime CreateDate { get; set; }
+
+    public DateTime? UpdateDate { get; set; }
+
+    public string CreatedBy { get; set; } = null!;
+
+    public string UpdatedBy { get; set; } = null!;
+
+    public bool IsActive { get; set; }
+}
